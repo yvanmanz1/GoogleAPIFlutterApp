845c6317219953d5920993cb3269280c77765bca
Yvan Manzi <113548523+yvanmanz1@users.noreply.github.com>
2024-02-20 17:33:47 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,12 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-// import 'firebase_options.dart';
+ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp(
-  //  options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(CalculatorApp());
 }
