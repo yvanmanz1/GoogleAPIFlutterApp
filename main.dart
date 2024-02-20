2d7b8366b22c05d4367ee544b5ee7d0eec3114c3
Yvan Manzi <113548523+yvanmanz1@users.noreply.github.com>
2024-02-20 17:34:12 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
- import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
