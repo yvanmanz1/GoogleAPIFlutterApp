075749f1592517464e00ea1314c8d7b716b23b56
Yvan Manzi <113548523+yvanmanz1@users.noreply.github.com>
2024-02-20 17:12:50 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,381 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+// import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(
+  //  options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(), // Set the default light theme
+      darkTheme: ThemeData.dark(), // Set the dark theme
+      themeMode:
+          ThemeMode.system, // Set to ThemeMode.dark for default dark mode
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_page),
+            label: 'Contact',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          if (index == 0) {
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CalculatorScreen()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ContactScreen()),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class ContactScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact'),
+      ),
+      body: Center(
+        child: Text('manziy20@gmail.com'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_page),
+            label: 'Contact',
+          ),
+        ],
+        currentIndex: 2,
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CalculatorScreen()),
+            );
+          } else if (index == 2) {}
+        },
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _input = '';
+  double _result = 0.0;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      height: MediaQuery.of(context)
+          .size
+          .height, // Set to approximate mobile screen height
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                _input,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+              ],
+            ),
+
+            _buildGoogleSignInButton(), // Add this line to include the Google Sign-In button
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _clear,
+              child: Text('Clear'),
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Container(
+                  height: 80, // Set a height that makes sense for your design
+                  child: const Text(
+                    'Drawer Header',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Home'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Contact'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ContactScreen()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calculate),
+              label: 'Calculator',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page),
+              label: 'Contact',
+            ),
+          ],
+          currentIndex: 1,
+          onTap: (index) {
+            if (index == 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+            } else if (index == 1) {
+              // Stay on the Calculator page
+            } else if (index == 2) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ContactScreen()),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGoogleSignInButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        UserCredential? userCredential = await signInWithGoogle();
+
+        if (userCredential != null) {
+          // User signed in successfully
+          print(
+              'User signed in with Google: ${userCredential.user?.displayName}');
+        } else {
+          // Handle sign-in failure or user cancellation
+          print('Google Sign-In failed');
+        }
+      },
+      child: Text('Sign In with Google'),
+    );
+  }
+
+  Widget _buildButton(String buttonText) {
+    return ElevatedButton(
+      onPressed: () {
+        _onButtonPressed(buttonText);
+      },
+      child: Text(buttonText),
+    );
+  }
+
+  void _onButtonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == '=') {
+        _calculateResult();
+      } else {
+        _input += buttonText;
+      }
+    });
+  }
+
+  void _calculateResult() {
+    try {
+      Parser p = Parser();
+      Expression exp = p.parse(_input);
+      ContextModel cm = ContextModel();
+      _result = exp.evaluate(EvaluationType.REAL, cm);
+      _input = _result.toString();
+    } catch (e) {
+      _input = 'Error';
+    }
+  }
+
+  void _clear() {
+    setState(() {
+      _input = '';
+      _result = 0.0;
+    });
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+
+       ConnectivityResult result = await Connectivity().checkConnectivity();
+
+      if (result == ConnectivityResult.none) {
+        
+        print('No internet connection');
+        return null;
+      }
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      if (googleUser == null) {
+        // User canceled the sign-in process
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Change GoogleAuthCredential to OAuthCredential
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Change _auth.signInWithCredential to _auth.signInWithCredential(credential)
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      print('Error during Google Sign-In: $e');
+      return null;
+    }
+  }
+}
